c3dfabb80ab6f7704eeb56f0413db2b4526af044
ChavinduShirantha <chavindushirantha@gmail.com>
2024-08-10 15:17:21 +0530

    Learn About Boxing

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class SampleTC
+{
+    public static void Main(string[] args)
+    {
+        //assigned int value
+        //2021 to n
+        int n = 2021;
+
+        //Boxing
+        object obj = n;
+
+        //value of n to be change 500
+        n = 500;
+
+        System.Console.WriteLine("Value type of is n is : {0}", n);
+        System.Console.WriteLine("Object/Reference type of is obj is : {0}",obj);
+    }
+}
