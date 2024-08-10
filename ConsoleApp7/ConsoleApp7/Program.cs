bcc6053cfd8ed3dee2a09d82c26c78bc0cd32dba
ChavinduShirantha <chavindushirantha@gmail.com>
2024-08-10 15:31:09 +0530

    Learn About Unboxing

--- ConsoleApp7/ConsoleApp7/Program.cs
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Unboxing
+{
+    public static void Main(string[] args)
+    {
+        //assigned int value
+        //25 to n
+        int n = 25;
+
+        //Boxing
+        object obj = n;
+
+        //Unboxing
+        int i = (int)obj;
+
+        //value of n to be change 500
+        n = 500;
+
+        System.Console.WriteLine("Value of obj object is : {0}", obj);
+        System.Console.WriteLine("Value of i  is : {0}", i);
+    }
+}
