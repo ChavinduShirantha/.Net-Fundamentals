c84a37041156b7edabded2cca185d33890f78e3f
ChavinduShirantha <chavindushirantha@gmail.com>
2024-08-10 14:57:20 +0530

    Learn About Implicit Type Conversion

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ITC
+{
+    class SampleTC
+    {
+        public static void Main(string[] args)
+        {
+            int i = 57;
+
+            //implicit type conversion
+            long l = i;
+
+            //implicit type conversion
+            float f = l;
+
+            //Final Result
+            Console.WriteLine("Integer Value" + i);
+            Console.WriteLine("Long Value" + l);
+            Console.WriteLine("Float Value" + f);
+        }
+    }
+}
