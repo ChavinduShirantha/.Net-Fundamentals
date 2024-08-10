68acb5eb7400e6d8ad55e5caa469810db454961c
ChavinduShirantha <chavindushirantha@gmail.com>
2024-08-10 15:02:35 +0530

    Learn About Explicit type Conversion

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ETC
+{
+    class SampleETC
+    {
+        public static void Main(string[] args)
+        {
+            double d = 54.123;
+
+            //Explicit type conversion
+            int i = (int)d;
+
+            //Final Result
+            Console.WriteLine("Value of i is: " + i);
+        }
+    }
+}
