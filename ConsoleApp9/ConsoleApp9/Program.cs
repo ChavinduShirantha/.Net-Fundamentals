fbeda8ea39ea589ab03909388a6ae76500993b62
ChavinduShirantha <chavindushirantha@gmail.com>
2024-08-11 17:00:26 +0530

    Learn About Conditional Statement

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Conditional
+{
+	class IfStatement
+	{
+		public static void Main(string[] args)
+		{
+			int num = 15;
+			if (num < 10)
+			{
+				Console.WriteLine("{0} is Less than 10", num);
+			}
+            Console.WriteLine("This statement is always executed");
+        }
+	}
+}
